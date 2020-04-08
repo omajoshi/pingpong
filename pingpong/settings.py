0bceedb476956f286a2c22635df3ee9b9bb37d8f
Om Joshi <omajoshi9@gmail.com>
2020-04-08 19:36:46 +0000

    decentralized settings

--- pingpong/settings.py
+++ pingpong/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .secret_settings import SECRET_KEY_SECRET
+from .secret_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,13 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ["pingpong.omajoshi.com"]
 
 
 # Application definition
@@ -119,10 +115,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 CSRF_COOKIE_SECURE = False
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
-SECURE_SSL_REDIRECT = True
